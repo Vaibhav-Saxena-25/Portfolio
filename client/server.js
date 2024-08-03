88f0b7dd6e0c78b198c86752abe3abb8654a18c9
Vaibhav-Saxena-25 <vaibhavsaxena25j@gmail.com>
2024-08-03 21:47:15 +0530

    Update server.js
    done

--- client/server.js
+++ client/server.js
@@ -10,19 +10,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.use('/api/v1/portfolio',require('./routes/portfolioRoutes'));
+app.use('/api/v1/portfolio', require('./routes/portfolioRoutes'));
 
-app.get('*',function(req,res)=>{
-    res.sendFile(path.join(__dirname,'../client/build/index.html'));
+app.get('*', function(req, res) {  // Corrected line
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('<h1>Welcome to Node Server</h1>');
-})
+});
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
